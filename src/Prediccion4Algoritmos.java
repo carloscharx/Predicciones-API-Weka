51cd7f67c52726d48bce94fc1db55d01f0320909
carloscharx <carlos.mediavilla@alumnos.uva.es>
2016-10-17 11:13:30 +0200

    Trabajo con la API de Weka realizado hasta la fecha de 18 de octubre de 2016

--- src/Prediccion4Algoritmos.java
+++ src/Prediccion4Algoritmos.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import weka.classifiers.functions.SMOreg;
 import weka.classifiers.lazy.IBk;
+import weka.core.EuclideanDistance;
+import weka.core.ManhattanDistance;
 import weka.classifiers.trees.REPTree;
 import weka.classifiers.trees.RandomTree;
 import weka.core.Instances;
@@ -13,6 +15,8 @@ import weka.classifiers.evaluation.NumericPrediction;
 import weka.classifiers.timeseries.WekaForecaster;
 import weka.classifiers.timeseries.eval.MAEModule;
 //import weka.classifiers.timeseries.core.TSLagMaker; esto parece que ya no existe, existe esto:
+import weka.core.MinkowskiDistance;
+import weka.core.neighboursearch.LinearNNSearch;
 import weka.filters.supervised.attribute.TSLagMaker;
 
 /**
@@ -34,15 +38,14 @@ public class Prediccion4Algoritmos {
             int algorithms_num = 4;
             double[][] errores_acumulados = new double[algorithms_num][links_num];
             for(int i = 0;i<algorithms_num;i++) {
-                PrintWriter writer = new PrintWriter("C:/Users/carloscharx/Documentos/Teleco/4º Teleco/Prácticas y TFG/datos-Funkfeuer-CONFINE/resultadosBis" + algoritmos[i] + ".txt");
+                PrintWriter writer = new PrintWriter("C:/Users/carloscharx/Documentos/Teleco/4º Teleco/Prácticas y TFG/datos-Funkfeuer-CONFINE/resultados2" + algoritmos[i] + ".txt");
                 for (int j = 0; j < links_num; j++) {
-                    // rutas de los datos de entrenamiento y test
-                    String pathToDataTraining = "C:/Users/carloscharx/Documentos/Teleco/4º Teleco/Prácticas y TFG/datos-Funkfeuer-CONFINE/datosWekaSeparados/link" + j + "training.arff";
-                    String pathToDataTest = "C:/Users/carloscharx/Documentos/Teleco/4º Teleco/Prácticas y TFG/datos-Funkfeuer-CONFINE/datosWekaSeparados/link" + j + "test.arff";
+                    // rutas de los datos
+                    String pathToData = "C:/Users/carloscharx/Documentos/Teleco/4º Teleco/Prácticas y TFG/datos-Funkfeuer-CONFINE/datosWeka/link" + j + ".arff";
 
                     // se cargan los datos de entrenamiento y test
-                    Instances training_set = new Instances(new BufferedReader(new FileReader(pathToDataTraining)));
-                    Instances test_set = new Instances(new BufferedReader(new FileReader(pathToDataTest)));
+                    Instances data_set = new Instances(new BufferedReader(new FileReader(pathToData)));
+                    Instances training_set = new Instances(data_set,0,1728);
 
 
                     // Se crea un nuevo predictor
@@ -59,22 +62,26 @@ public class Prediccion4Algoritmos {
                             break;
                         }
                         case 1:
-                        {   String[] options ={"-N","1"};
+                        {   //String[] options ={"-N","1"};
                             SMOreg predictor = new SMOreg();
-                            predictor.setOptions(options);
+                            //predictor.setOptions(options);
                             forecaster.setBaseForecaster(predictor);
                             break;
                         }
                         case 2:
                         {
-                            forecaster.setBaseForecaster(new IBk());
+                            LinearNNSearch algoritmobusqueda=new LinearNNSearch();
+                            algoritmobusqueda.setDistanceFunction(new ManhattanDistance());
+                            IBk predictor = new IBk();
+                            predictor.setNearestNeighbourSearchAlgorithm(algoritmobusqueda);
+                            forecaster.setBaseForecaster(predictor);
                             break;
                         }
                         case 3:
                         {
-                            String[] options ={"-N","1"};
+                            //String[] options ={"-N","1"};
                             GaussianProcesses predictor = new GaussianProcesses();
-                            predictor.setOptions(options);
+                            //predictor.setOptions(options);
                             forecaster.setBaseForecaster(predictor);
                             break;
                         }
@@ -89,57 +96,63 @@ public class Prediccion4Algoritmos {
                     // Contruye el modelo
                     forecaster.buildForecaster(training_set, System.out);
 
-                    // prime the forecaster with enough recent historical data
-                    // to cover up to the maximum lag. In our case, we could just supply
-                    // the 12 most recent historical instances, as this covers our maximum
-                    // lag period
+                    double[] error_enlace = new double[288];
+                    for(int k=0;k<288;k++){
+                        // prime the forecaster with enough recent historical data
+                        // to cover up to the maximum lag. In our case, we could just supply
+                        // the 12 most recent historical instances, as this covers our maximum
+                        // lag period
+                        forecaster.primeForecaster(new Instances(data_set,1716+k,12));
+                        // Predice 1 unidad desde el fin del conjunto de datos con los que ha alimentado al predictor
+                        List<List<NumericPrediction>> forecast = forecaster.forecast(1, System.out);
 
-                    forecaster.primeForecaster(training_set);
+                        // Aquí hay que saturar los valores, descomentar cuando se use e intentar reducir codigo
 
-                    // Predice 288 unidades desde el fin del conjunto de entrenamiento
-                    List<List<NumericPrediction>> forecast = forecaster.forecast(288, System.out);
-
-
-                    // Aquí hay que saturar los valores,
-
-                    for (List<NumericPrediction> predAtStep : forecast) {
-                        for (NumericPrediction predForTarget : predAtStep) {
-                            if (predForTarget.predicted() > 1.0) {
-                                predAtStep.set(predAtStep.indexOf(predForTarget), new NumericPrediction(predForTarget.actual(), new Double(1)));
-                            } else if (predForTarget.predicted() < 0.0) {
-                                predAtStep.set(predAtStep.indexOf(predForTarget), new NumericPrediction(predForTarget.actual(), new Double(0)));
+                        /*for (List<NumericPrediction> predAtStep : forecast) {
+                            for (NumericPrediction predForTarget : predAtStep) {
+                                if (predForTarget.predicted() > 1.0) {
+                                    predAtStep.set(predAtStep.indexOf(predForTarget), new NumericPrediction(predForTarget.actual(), new Double(1)));
+                                } else if (predForTarget.predicted() < 0.0) {
+                                    predAtStep.set(predAtStep.indexOf(predForTarget), new NumericPrediction(predForTarget.actual(), new Double(0)));
+                                }
                             }
-                        }
-                        forecast.set(forecast.indexOf(predAtStep), predAtStep);
+                            forecast.set(forecast.indexOf(predAtStep), predAtStep);
+                        }*/
+
+                        // Ahora se imprimen, descomentar si fuese necesario
+
+                        /*for (int i = 0; i < 288; i++) {
+                            List<NumericPrediction> predsAtStep = forecast.get(i);
+                                for (int j = 0; j < 1; j++) {
+                            NumericPrediction predForTarget = predsAtStep.get(j);
+                            System.out.print("" + predForTarget.predicted() + " ");
+                            }
+                        System.out.println();
+                        }*/
+
+                        double[] error;
+
+                        List<String> fields = new ArrayList();
+                        fields.add("Link" + j);
+                        MAEModule calculoMae = new MAEModule();
+                        calculoMae.setTargetFields(fields);
+                        List<NumericPrediction> predsAtStep = forecast.get(0);
+                        calculoMae.evaluateForInstance(predsAtStep, data_set.get(1728+k));
+                        error = calculoMae.calculateMeasure();
+                        error_enlace[k]=error[0];
+
                     }
 
-                    // Ahora se imprimen, descomentar si fuese necesario
 
-                /*for (int i = 0; i < 288; i++) {
-                    List<NumericPrediction> predsAtStep = forecast.get(i);
-                    for (int j = 0; j < 1; j++) {
-                        NumericPrediction predForTarget = predsAtStep.get(j);
-                        System.out.print("" + predForTarget.predicted() + " ");
+
+                    double mae=0;
+                    for(int k=0;k<288;k++){
+                        mae+=error_enlace[k];
                     }
-                    System.out.println();
-                }*/
-                    double[] error;
+                    mae=mae/288;
+                    System.out.println("La MAE es " + mae);
+                    errores_acumulados[i][j]=mae;
 
-                    List<String> fields = new ArrayList();
-                    fields.add("Link" + j);
-                    MAEModule calculoMae = new MAEModule();
-                    calculoMae.setTargetFields(fields);
-
-                    for (int k = 0; k < 288; k++) {
-                        List<NumericPrediction> predsAtStep = forecast.get(k);
-                        calculoMae.evaluateForInstance(predsAtStep, test_set.get(k));
-                    }
-
-
-                    error = calculoMae.calculateMeasure();
-                    System.out.println();
-                    System.out.println(error[0]); // Cuidado, si imprimes el vector de double no se imprime bien
-                    errores_acumulados[i][j] = error[0];
                     double sum = 0.0;
                     for (int k = 0; k <= j; k++) {
                         sum += errores_acumulados[i][k];
