0102b1534ed365cbe6c29fbdf6c5c39b74340adf
carloscharx <carlos.mediavilla@alumnos.uva.es>
2016-10-24 12:41:53 +0200

    Trabajo con la API de Weka realizado hasta la fecha de 25 de octubre de 2016

--- src/PrediccionRTLag.java
+++ src/PrediccionRTLag.java
@@ -22,30 +22,31 @@ import weka.filters.supervised.attribute.TSLagMaker;
 /**
  * Example of using the time series forecasting API. To compile and
  * run the CLASSPATH will need to contain:
- *
+ * <p>
  * weka.jar (from your weka distribution)
  * pdm-timeseriesforecasting-ce-TRUNK-SNAPSHOT.jar (from the time series package)
  * jcommon-1.0.14.jar (from the time series package lib directory)
  * jfreechart-1.0.13.jar (from the time series package lib directory)
  */
-public class Prediccion4Algoritmos {
+public class PrediccionRTLag {
 
     public static void main(String[] args) {
         try {
             int links_num = 979; // Número de enlaces final obtenido
             // Los algortimos que vamos a usar:
-            String[] algoritmos= {"REPTRee", "SMOreg", "IBk", "GaussianProcesses"};
-            int algorithms_num = 4;
-            double[][] errores_acumulados = new double[algorithms_num][links_num];
-            for(int i = 0;i<algorithms_num;i++) {
-                PrintWriter writer = new PrintWriter("C:/Users/carloscharx/Documentos/Teleco/4º Teleco/Prácticas y TFG/datos-Funkfeuer-CONFINE/resultados" + algoritmos[i] + ".txt");
+            String[] algoritmos = {"REPTRee", "SMOreg", "IBk", "GaussianProcesses"};
+            int max_lag = 24;
+            double[][] errores_acumulados = new double[max_lag][links_num];
+
+            for (int lag = 23; lag < max_lag; lag++) {
+                PrintWriter writer = new PrintWriter("C:/Users/carloscharx/Documentos/Teleco/4º Teleco/Prácticas y TFG/datos-Funkfeuer-CONFINE/resultadosRTlag" + (lag+1) + ".txt");
                 for (int j = 0; j < links_num; j++) {
                     // rutas de los datos
                     String pathToData = "C:/Users/carloscharx/Documentos/Teleco/4º Teleco/Prácticas y TFG/datos-Funkfeuer-CONFINE/datosWeka/link" + j + ".arff";
 
                     // se cargan los datos de entrenamiento y test
                     Instances data_set = new Instances(new BufferedReader(new FileReader(pathToData)));
-                    Instances training_set = new Instances(data_set,0,1728);
+                    Instances training_set = new Instances(data_set, 0, 1728);
 
 
                     // Se crea un nuevo predictor
@@ -54,58 +55,24 @@ public class Prediccion4Algoritmos {
                     // Se elije que queremos predecir, en este caso el Link j-ésimo
                     forecaster.setFieldsToForecast("Link" + j);
 
-                    // Elegimos el algoritmo para cada una de las iteraciones
-                    switch (i){
-                        case 0:
-                        {
-                            forecaster.setBaseForecaster(new REPTree());
-                            break;
-                        }
-                        case 1:
-                        {   //String[] options ={"-N","1"};
-                            SMOreg predictor = new SMOreg();
-                            //predictor.setOptions(options);
-                            forecaster.setBaseForecaster(predictor);
-                            break;
-                        }
-                        case 2:
-                        {
-                            //LinearNNSearch algoritmobusqueda=new LinearNNSearch();
-                            //algoritmobusqueda.setDistanceFunction(new ManhattanDistance());
-                            //String[] options ={"-X"};
-                            int k=10;
-                            IBk predictor = new IBk(k);
-                            //predictor.setOptions(options);
-                            //predictor.setNearestNeighbourSearchAlgorithm(algoritmobusqueda);
-                            forecaster.setBaseForecaster(predictor);
-                            break;
-                        }
-                        case 3:
-                        {
-                            //String[] options ={"-N","1"};
-                            GaussianProcesses predictor = new GaussianProcesses();
-                            //predictor.setOptions(options);
-                            forecaster.setBaseForecaster(predictor);
-                            break;
-                        }
-                    }
 
+                    forecaster.setBaseForecaster(new REPTree());
 
 
                     forecaster.getTSLagMaker().setTimeStampField("timestamp"); // date time stamp
                     forecaster.getTSLagMaker().setMinLag(1);
-                    forecaster.getTSLagMaker().setMaxLag(12);
+                    forecaster.getTSLagMaker().setMaxLag(lag);
 
                     // Contruye el modelo
                     forecaster.buildForecaster(training_set, System.out);
 
                     double[] error_enlace = new double[288];
-                    for(int k=0;k<288;k++){
+                    for (int k = 0; k < 288; k++) {
                         // prime the forecaster with enough recent historical data
                         // to cover up to the maximum lag. In our case, we could just supply
                         // the 12 most recent historical instances, as this covers our maximum
                         // lag period
-                        forecaster.primeForecaster(new Instances(data_set,1716+k,12));
+                        forecaster.primeForecaster(new Instances(data_set, 1728 - lag + k, lag));
                         // Predice 1 unidad desde el fin del conjunto de datos con los que ha alimentado al predictor
                         List<List<NumericPrediction>> forecast = forecaster.forecast(1, System.out);
 
@@ -140,30 +107,29 @@ public class Prediccion4Algoritmos {
                         MAEModule calculoMae = new MAEModule();
                         calculoMae.setTargetFields(fields);
                         List<NumericPrediction> predsAtStep = forecast.get(0);
-                        calculoMae.evaluateForInstance(predsAtStep, data_set.get(1728+k));
+                        calculoMae.evaluateForInstance(predsAtStep, data_set.get(1728 + k));
                         error = calculoMae.calculateMeasure();
-                        error_enlace[k]=error[0];
+                        error_enlace[k] = error[0];
 
                     }
 
 
-
-                    double mae=0;
-                    for(int k=0;k<288;k++){
-                        mae+=error_enlace[k];
+                    double mae = 0;
+                    for (int k = 0; k < 288; k++) {
+                        mae += error_enlace[k];
                     }
-                    mae=mae/288;
+                    mae = mae / 288;
                     System.out.println("La MAE es " + mae);
-                    errores_acumulados[i][j]=mae;
+                    errores_acumulados[lag][j] = mae;
 
                     double sum = 0.0;
                     for (int k = 0; k <= j; k++) {
-                        sum += errores_acumulados[i][k];
+                        sum += errores_acumulados[lag][k];
                     }
                     sum = sum / (j + 1);
                     System.out.println(sum);
                     System.out.println("Fin de la iteración número " + (j + 1));
-                    writer.println(errores_acumulados[i][j]);
+                    writer.println(errores_acumulados[lag][j]);
                 }
                 writer.close();
             }
